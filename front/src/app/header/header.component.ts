fd4a1959cdf7044b6b362f8411e0dc147544c9f7
Nakutaka <alexandre.col@etu.univ-cotedazur.fr>
2020-03-24 14:59:40 +0100

    Modification du header : changement du titre en fct de l'url

--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { print } from 'util';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  title(){
+    var coupeDecale = this.router.url.split('/')[1];
+    console.log(coupeDecale);
+    var oeoe: string;
+    switch(coupeDecale) {
+      case "quiz-list":{
+        oeoe = "Mosaïque";
+        console.log
+        break; 
+      }
+      case "edit-quiz":{
+        oeoe = "Edition de Quiz"
+        break;
+      }
+    }
+    return oeoe;
+  }
+
+
+  ngOnInit() {   
+    console.log("Url : "+this.router.url);
+    console.log("Full route : "+window.location.href);
   }
 
 }
