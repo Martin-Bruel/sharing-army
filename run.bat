f6efcd14f02aa8f3aef67a4b4464c6bb6fc616cd
YannLeon <yann.leon@etu.unice.fr>
2020-04-18 16:45:41 +0200

    G TOU CASSAIT

--- run.bat
+++ run.bat
@@ -3,6 +3,7 @@ Xcopy "mocks\*.*" "back\mocks\" /K /D /H /Y
 cd back
 start cmd /K npm run dev
 cd../front
+start cmd /K set GOOGLE_APPLICATION_CREDENTIALS=My First Project-f173b178df81.json
 start cmd /K ng serve
 start code
 exit
